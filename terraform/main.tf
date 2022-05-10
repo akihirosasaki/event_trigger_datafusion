60eb9ed06075ffc681fddc7b8ef3628984486de1
akihirosasaki <akihirosasaki0730@gmail.com>
2022-05-10 23:06:19 +0900

    add 必要なインフラコードを追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,80 @@
-# data fusionの設定
-module "data_fusion" "data_fusion_instance"{
-  source = "terraform-google-modules/data-fusion/google"
-  version = "~> 0.1"
-
-  name = "sasakky_datafusion_instance"
+# サービスアカウント作成とロールの付与
+resource "google_service_account" "develop" {
+  project = var.GCP_PROJECT_ID
+  account_id = "develop"
+  display_name = "develop"
 }
 
-module "sandbox" {
-  source = "terraform-google-modules/data-fusion/google//modules/namespace"
-  version = "~> 0.1"
+resource "google_project_iam_member" "develop_cloudfunction_invoker" {
+  project = var.GCP_PROJECT_ID
+  role = "roles/cloudfunctions.invoker"
+  member = "serviceAccount:${google_service_account.develop.email}"
+}
 
+resource "google_project_iam_member" "develop_datafusion_admin" {
+  project = var.GCP_PROJECT_ID
+  role = "roles/datafusion.admin"
+  member = "serviceAccount:${google_service_account.develop.email}"
+}
+
+resource "google_project_iam_member" "develop_storage_admin" {
+  project = var.GCP_PROJECT_ID
+  role = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.develop.email}"
+}
+
+resource "google_project_iam_member" "develop_bigquery_editer" {
+  project = var.GCP_PROJECT_ID
+  role = "roles/bigquery.dataEditor"
+  member = "serviceAccount:${google_service_account.develop.email}"
+}
+
+resource "google_project_iam_member" "develop_bigquery_jobUser" {
+  project = var.GCP_PROJECT_ID
+  role = "roles/bigquery.jobUser"
+  member = "serviceAccount:${google_service_account.develop.email}"
+}
+
+resource "google_project_iam_member" "develop_serviceAccountUser" {
+  project = var.GCP_PROJECT_ID
+  role = "roles/iam.serviceAccountUser"
+  member = "serviceAccount:${google_service_account.develop.email}"
+}
+
+resource "google_project_iam_member" "develop_dataproc_worker" {
+  project = var.GCP_PROJECT_ID
+  role = "roles/dataproc.worker"
+  member = "serviceAccount:${google_service_account.develop.email}"
+}
+
+resource "google_project_iam_member" "sa-datafusion_serviceAccountUser" {
+  project = var.GCP_PROJECT_ID
+  role = "roles/iam.serviceAccountUser"
+  member = "serviceAccount:service-840090550767@gcp-sa-datafusion.iam.gserviceaccount.com"
+}
+
+
+# data fusionの設定
+resource "google_data_fusion_instance" "instance" {
+  name = "sasakky-datafusion-instance"
+  project = var.GCP_PROJECT_ID
+  region = var.REGION
+  type = "BASIC"
+  dataproc_service_account = google_service_account.develop.email
+}
+
+resource "cdap_namespace" "namespace" {
   name = "sandbox"
+}
+
+resource "cdap_application" "pipeline" {
+  name = "weekly_survey_etl"
+  namespace = cdap_namespace.namespace.name
+  spec = file("../src/data_fusion/weekly_survey_etl-cdap-data-pipeline.json")
+
+  depends_on = [
+    cdap_namespace.namespace
+  ]
 }
 
 # cloud functionsの設定
@@ -27,7 +91,7 @@ resource "google_storage_bucket" "input_bucket" {
 # Generates an archive of the source code compressed as a .zip file.
 data "archive_file" "source" {
     type        = "zip"
-    source_dir  = "../src/weekly_survey_etl_function"
+    source_dir  = "../src/cloud_function"
     output_path = "/tmp/function.zip"
 }
 
@@ -59,14 +123,15 @@ resource "google_cloudfunctions_function" "function" {
 
     # Must match the function name in the cloud function `main.py` source code
     entry_point           = "run_job"
+    service_account_email = google_service_account.develop.email
 
     environment_variables = {
-      INSTANCE_ID = data_fusion.data_fusion_instance.name,
+      INSTANCE_ID = google_data_fusion_instance.instance.name,
       REGION = var.REGION,
-      PIPELINE_NAME = "",
+      PIPELINE_NAME = cdap_application.pipeline.name,
       PROJECT_ID = var.GCP_PROJECT_ID,
-      NAMESPACE_ID = sandbox.name,
-      CDAP_ENDPOINT = ""
+      NAMESPACE_ID = cdap_namespace.namespace.name,
+      CDAP_ENDPOINT = "https://${google_data_fusion_instance.instance.name}-${var.GCP_PROJECT_ID}-dot-usc1.datafusion.googleusercontent.com/api"
     }
     
     # 
@@ -78,6 +143,10 @@ resource "google_cloudfunctions_function" "function" {
     # Dependencies are automatically inferred so these lines can be deleted
     depends_on            = [
         google_storage_bucket.function_bucket,  # declared in `storage.tf`
-        google_storage_bucket_object.zip
+        google_storage_bucket_object.zip,
+        google_data_fusion_instance.instance,
+        cdap_namespace.namespace,
+        cdap_application.pipeline,
+        google_service_account.develop
     ]
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,20 @@ provider "google" {
   region      = var.REGION
   credentials = "${file(var.GOOGLE_APPLICATION_CREDENTIALS)}"
 }
+
+terraform {
+  required_providers {
+    cdap = {
+      source = "GoogleCloudPlatform/cdap"
+      # Pin to a specific version as 0.x releases are not guaranteed to be backwards compatible.
+      version = "0.9.0"
+    }
+  }
+}
+
+data "google_client_config" "current" {}
+
+provider "cdap" {
+  host  = "${google_data_fusion_instance.instance.service_endpoint}/api/"
+  token = data.google_client_config.current.access_token
+}
